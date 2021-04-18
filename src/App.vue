2785907baaad8941029bfd3f4ea47a8c0425337f
lvheng <1834805770@qq.com>
2021-04-18 20:38:49 +0800

    初始化项目依赖

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <do-button>我是按钮</do-button>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'do-button': Button
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
